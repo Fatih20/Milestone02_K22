c4e512bd8e5111ebf76eff9af4fe075289a0a4e5
Fatih20 <fnri39@protonmail.com>
2022-08-16 10:25:40 +0700

    Styling of deactivation page.

--- src/components/elements/deactivation.tsx
+++ src/components/elements/deactivation.tsx
@@ -4,22 +4,35 @@ const Main = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
   justify-content: center;
+`;
+
+const Title = styled.h2`
+  font-weight: 900;
+  font-size: 4em;
+  text-align: center;
+`;
+
+const Subtitle = styled.h3`
+  color: var(--yellow-color);
+  font-weight: 900;
+  font-size: 3em;
 `;
 
 export default function Deactivation({
   hour,
   minute,
 }: {
-  hour: number;
-  minute: number;
+  hour: number | undefined;
+  minute: number | undefined;
 }) {
   return (
     <Main>
-      <p>
-        Alarm at {hour}.{minute}
-      </p>
-      <p>Deactivated</p>
+      <Title>
+        Alarm at {hour ?? "00"}.{minute ?? "00"}
+      </Title>
+      <Subtitle>Deactivated</Subtitle>
     </Main>
   );
 }
